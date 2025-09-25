d3d16b2a7dad19d431b03793e7075023498c0cc5
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-09-25 21:11:27 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

115aef08f284c89e79c45b77f34d654bd240309f
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-09-28 21:20:00 +0200

    first why-me design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
+import { WhyMeComponent } from "./why-me/why-me.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent],
+  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

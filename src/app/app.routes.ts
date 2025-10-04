762680b64081b3488aff5e8bfe7d9f42984a7a1f
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-10-04 23:37:19 +0200

    navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: AboveTheFoldComponent}
+];

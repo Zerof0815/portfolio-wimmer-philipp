e5549f797691d737ee8521cbe0fc7cbec1c60ba5
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-10-11 11:58:29 +0200

    repaired the header and worked on my-projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
 import { WhyMeComponent } from "./why-me/why-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { MyProjectsComponent } from "./my-projects/my-projects.component";
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
+  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'wimmer-philipp';
+
+  constructor(private viewportScroller: ViewportScroller) {
+    viewportScroller.setOffset([0, 160]);
+  }
 }
--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgClass } from '@angular/common';
+import { JoinComponent } from "./projects/join/join.component";
+import { ElPolloLocoComponent } from "./projects/el-pollo-loco/el-pollo-loco.component";
+import { OngoingProjectsComponent } from "./projects/ongoing-projects/ongoing-projects.component";
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [],
+  imports: [NgClass, CommonModule, JoinComponent, ElPolloLocoComponent, OngoingProjectsComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent {
+  projects = [
+    { id: 1, name: 'Join' },
+    { id: 2, name: 'El Pollo Loco Space' },
+    { id: 3, name: 'Ongoing Project' }
+  ];
 
+  selectedProjectId = 1;
+
+  selectProject(id: number) {
+    this.selectedProjectId = id;
+  }
 }

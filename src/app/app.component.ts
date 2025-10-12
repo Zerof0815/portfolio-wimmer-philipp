733ebe5e7cd68b4e427eaeeb8d20959ff8a69f75
Wimmer Philipp <wimmer.philipp@gmx.at>
2025-10-12 16:13:40 +0200

    worked on my-projects and created footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { WhyMeComponent } from "./why-me/why-me.component";
 import { MySkillsComponent } from "./my-skills/my-skills.component";
 import { MyProjectsComponent } from "./my-projects/my-projects.component";
 import { HeaderComponent } from "./shared/header/header.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, AboveTheFoldComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

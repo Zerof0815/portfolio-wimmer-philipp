89e10d203882b3fd8fe3722744963ec5657f5940
Wimmer Philipp <wimmer.philipp@gmx.at>
2026-02-10 18:54:20 +0100

    worked on responsive mobile/header/app.component

--- src/app/above-the-fold/mobile/mobile.component.ts
+++ src/app/above-the-fold/mobile/mobile.component.ts
@@ -1,13 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mobile',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './mobile.component.html',
   styleUrl: './mobile.component.scss'
 })
 export class MobileComponent {
+  menuOpen = false;
+  imageSource = "assets/img/atf/menu-button.png";
+  currentLang: 'DE' | 'EN' = 'DE';
 
+  setLanguage(lang: 'DE' | 'EN') {
+    this.currentLang = lang;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    if (this.menuOpen === true) {
+      this.imageSource = "assets/img/atf/close-button.png";
+    } else {
+      this.imageSource = "assets/img/atf/menu-button.png";
+    }
+  }
 }

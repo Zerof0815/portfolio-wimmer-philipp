5252397daedfb6c4b55d16786257e6d4651e0dae
Wimmer Philipp <wimmer.philipp@gmx.at>
2026-03-02 20:53:21 +0100

    all buttons and links work

--- src/app/why-me/why-me.component.ts
+++ src/app/why-me/why-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-why-me',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './why-me.component.html',
   styleUrl: './why-me.component.scss'
 })
